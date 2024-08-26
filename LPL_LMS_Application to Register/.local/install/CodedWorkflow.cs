05e5d2516b802f6420a256b37d19f0ea824fb542
ambulurimahesh <113670136+ambulurimahesh@users.noreply.github.com>
2024-08-26 13:57:59 +0100

    updated codes

---
+++ LPL_LMS_Application to Register/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace LPL_LMS_Applicationtoregister
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
